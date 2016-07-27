3136b99641f238e745da7004fb30886c864e745c
Rigo Rojas <rigo@Rigos-MacBook.fios-router.home>
2016-07-27 02:13:36 -0700

    whitelist domain in info.plist

--- index.ios.js
+++ index.ios.js
@@ -6,25 +6,37 @@ import {
   Text,
   View
 } from 'react-native';
-import Api from './src/api';
 
 class weather extends Component {
+
   state = {
     pin: {
       latitude: 0,
       longitude: 0
-    }
+    },
+    city: '',
+    temperature: '',
+    description: ''
   };
+
   render() {
     return (
-      <MapView
-        annotations={[this.state.pin]}
-        onRegionChangeComplete={this.onRegionChangeComplete}
-        style={styles.map}
-      >
-      </MapView>
+      <View style={styles.container}>
+        <MapView
+          annotations={[this.state.pin]}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+          style={styles.map}
+        >
+        </MapView>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>{this.state.city}</Text>
+          <Text style={styles.text}>{this.state.temperature}</Text>
+          <Text style={styles.text}>{this.state.description}</Text>
+        </View>
+      </View>
     );
   }
+
   onRegionChangeComplete = (region) => {
     this.setState({
       pin: {
@@ -32,12 +44,48 @@ class weather extends Component {
         longitude: region.longitude
       }
     });
+    this.Api(region.latitude, region.longitude)
+    .then((data) => {
+      this.setState(data);
+    });
   };
+
+  Api = (latitude, longitude) => {
+    const rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=30d4f5b2038a8b73139044f69026e30c';
+    const url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+    return fetch(url)
+      .then((response) => {
+          return response.json();
+      })
+      .then((json) => {
+        return {
+          city: json.name,
+          temperature: json.main.temp,
+          description: json.weather[0].description
+        }
+      })
+      .catch((err) => console.log("Api", err));
+  }
+
 }
 
 const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FCFF'
+  },
   map: {
-    flex: 1
+    flex: 2,
+    marginTop: 30
+  },
+  textWrapper: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 30
   }
 });
 
--- src/api.js
+++ src/api.js
@@ -1,22 +1,25 @@
-let rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=30d4f5b2038a8b73139044f69026e30c';
+"use strict";
 
-let kelvinToF = () => {
-    return Math.round((kelvin - 273.15) * 1.8 +32) + ' ˚F'
-};
+const rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=30d4f5b2038a8b73139044f69026e30c';
 
-let Api = (latitude, longitude) => {
-    let url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((json) => {
-            return {
-                name: json.name,
-                temperature: kelvinToF(json.main.temp),
-                description: json.weather[0].description
-            }
-        });
+// let kelvinToF = () => {
+//   return Math.round((kelvin - 273.15) * 1.8 +32) + ' ˚F';
+// };
+
+module.exports = function Api(latitude, longitude) {
+  const url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+  return fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+      return {
+        city: json.name,
+        //temperature: kelvinToF(json.main.temp),
+        temperature: json.main.temp,
+        description: json.weather[0].description
+      }
+    }
+  )
+  .catch((err) => console.log("Api", err));
 }
-
-export default Api;
