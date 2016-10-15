dfd09a2aa43877b7f85a6f504e76521168bb0807
Rigo Rojas <rigo.rojas@nfl.com>
2016-10-14 17:58:34 -0700

    Created Temperature View

--- index.ios.js
+++ index.ios.js
@@ -7,19 +7,11 @@ import {
   View
 } from 'react-native';
 import Drawer from 'react-native-drawer'; //source: https://github.com/root-two/react-native-drawer
-import Api from "./src/api";
-import {ControlPanel} from "./src/ControlPanel";
+import {ControlPanel} from "./src/Components/ControlPanel/ControlPanel"; //source: https://github.com/root-two/react-native-drawer
+import Temperature from "./src/Views/Temperature/Temperature";
 
 class weather extends Component {
-  state = {
-    pin: {
-      latitude: 0,
-      longitude: 0
-    },
-    city: '',
-    temperature: '',
-    description: ''
-  };
+  state = {};
 
   closeControlPanel = () => {
     this._drawer.close()
@@ -36,55 +28,10 @@ class weather extends Component {
             openDrawerOffset={100}
             content={<ControlPanel closeDrawer={this.closeControlPanel} />}
         >
-          <View style={styles.container}>
-            <MapView
-              annotations={[this.state.pin]}
-              onRegionChangeComplete={this.onRegionChangeComplete}
-              style={styles.map}
-            >
-            </MapView>
-            <View style={styles.textWrapper}>
-              <Text style={styles.text}>{this.state.city}</Text>
-              <Text style={styles.text}>{this.state.temperature}</Text>
-              <Text style={styles.text} onPress={this.openControlPanel}>{this.state.description}</Text>
-            </View>
-          </View>
+            <Temperature openDrawer={this.openControlPanel} />
         </Drawer>
     );
   }
-
-  onRegionChangeComplete = (region) => {
-    this.setState({
-      pin: {
-        latitude: region.latitude,
-        longitude: region.longitude
-      }
-    });
-    Api(region.latitude, region.longitude)
-    .then((data) => {
-      this.setState(data);
-    });
-  };
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF'
-  },
-  map: {
-    flex: 2,
-    marginTop: 30
-  },
-  textWrapper: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  text: {
-    fontSize: 30
-  }
-});
 
 AppRegistry.registerComponent('weather', () => weather);
